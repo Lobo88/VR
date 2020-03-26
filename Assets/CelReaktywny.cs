54a100f4400c91c7e54100a9b0f53458739b551e
Tomasz Gancarz <tomasz.gancarz.wro@gmail.com>
2020-03-26 11:32:07 +0100

    Unity

---
+++ Assets/CelReaktywny.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelReaktywny : MonoBehaviour
+{
+    public void ReactToHit() { 
+        BaseAi behavior = GetComponent<BaseAi>();
+        if (behavior != null) {
+            behavior.SetAlive(false); 
+        } StartCoroutine(Die()); 
+    }
+
+    private IEnumerator Die() { 
+        this.transform.Rotate(-90, 0, 0);
+        transform.localPosition= new Vector3(transform.localPosition.x, 0, transform.localPosition.z);
+        yield return new WaitForSeconds(1.5f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+
+    }
+}
